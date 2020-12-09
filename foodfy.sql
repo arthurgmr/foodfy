99c2d61761c70f013370954f4f9cf6f5152eaab5
arthurgmr <arthur.gmr@hotmail.com>
2020-12-09 12:07:03 -0300

    foodfy

--- foodfy.sql
+++ foodfy.sql
@@ -1,13 +1,18 @@
+--creating database;
+CREATE DATABASE foodfy;
+
+--creating tables;
 CREATE TABLE "recipes" (
   "id" SERIAL PRIMARY KEY,
-  "title" text,
-  "chef_id" int,
+  "title" text NOT NULL,
+  "chef_id" int NOT NULL,
   "featured" text,
   "homepage" text,
-  "ingredients" text[],
+  "ingredients" text[] NOT NULL,
   "preparation" text[],
   "information" text,
   "created_at" timestamp DEFAULT (now())
+  "updated_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "chefs" (
@@ -18,6 +23,7 @@ CREATE TABLE "chefs" (
   "email" text,
   "file_id" int NOT NULL,
   "created_at" timestamp DEFAULT (now())
+  "updated_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "files" (
@@ -35,3 +41,24 @@ CREATE TABLE "recipe_files" (
 ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
 
 ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
+
+--creating procedure
+CREATE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN 
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+--trigger to auto updated_at in recipes
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON recipes
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+--trigger to auto updated_at in chefs
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON chefs
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
